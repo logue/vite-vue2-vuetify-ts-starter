7cd4485c71118fad4d8af17a304fed5b021aff64
Logue <logue@hotmail.co.jp>
2022-11-02 09:53:36 +0900

    Fixed env.d.ts not commit. Fixed deploy-gh-pages.yml for deploy command change.

---
+++ env.d.ts
@@ -0,0 +1,28 @@
+/* eslint-disable spaced-comment */
+/// <reference types="vite/client" />
+/// <reference types="node" />
+/// <reference types="vuetify" />
+
+// Vue
+declare module '*.vue' {
+  import Vue from 'vue';
+  export default Vue;
+}
+
+// Vuetify
+declare module 'vuetify/lib/framework' {
+  import Vuetify from 'vuetify';
+  export default Vuetify;
+}
+
+interface ImportMetaEnv {
+  // see https://vitejs.dev/guide/env-and-mode.html#env-files
+  // add .env variables.
+  readonly VITE_APP_TITLE: string;
+  readonly VITE_APP_WEBSTORAGE_NAMESPACE: string;
+}
+
+// eslint-disable-next-line no-unused-vars
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
