97c434e2eee1fb43cc7454105c3b8feceef985a6
Logue <logue@hotmail.co.jp>
2023-02-16 19:31:44 +0900

    Update dependencies. Temporarily disable vue-tsc for vite-plugin-checker.

--- vite.config.ts
+++ vite.config.ts
@@ -48,7 +48,7 @@ export default defineConfig(async ({ command, mode }): Promise<UserConfig> => {
       // https://github.com/fi3ework/vite-plugin-checker
       checker({
         typescript: true,
-        vueTsc: true,
+        // vueTsc: true,
         eslint: {
           lintCommand:
             'eslint --cache --cache-location ./node_modules/.vite/vite-plugin-eslint', // for example, lint .ts & .tsx
